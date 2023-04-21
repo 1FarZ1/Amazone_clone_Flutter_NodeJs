fc26496eaef70e67f0a2af572c04626ec23c8b06
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-21 16:19:17 +0200

    adding jwt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,17 +2,27 @@ import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 
 // hada howa controller t3 auth li yhdr m3a auth , drnah hna for better architecture 
-let  signIn= (req, res) => {
+let  signIn= async (req, res) => {
     const {email , password} = req.body;
     try {
         const  existingUser = User.findOne({email});
         if(!existingUser){
             return res.status(404).json({msg:"User Doesn't Exist"});
         }   
+        bcrypt.compare(password, existingUser.password, (err, result) => {
+            if (err) {
+                return res.status(404).json({ msg: "Error" + err });
+            }
+            if (!result) {
+                return res.status(404).json({ msg: "Password Doesn't Match" });
+            }
+        })
+
         return res.status(200).json({
             msg:"User Signed In Successfully",
             user:existingUser
         })
+
 
         
     } catch (error) {
