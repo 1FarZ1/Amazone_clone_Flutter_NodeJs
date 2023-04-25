a146a54b4b78b4e94588e4b3dd67b09531afe446
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-25 21:35:31 +0200

    added order model

---
+++ server/models/order.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+import { productSchema } from "./product";
+
+const orderSchema = Schema({
+  products: [
+    {
+      product: productSchema,
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    required: true,
+    type: String,
+  },
+  orderedAt: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Order = model("Order", orderSchema);
+export default Order;
