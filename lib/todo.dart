9230f8d29f5a74a7ae749a64708438721de2e568
FarZ <91225280+1FarZ1@users.noreply.github.com>
2023-06-09 09:51:00 -0800

    Delete todo.dart

--- lib/todo.dart
+++
@@ -1,4 +0,0 @@
-// think for a solution for the rating bug, maybe you will redesign the whole system, i think this issue can be sovled with creating a global provider for rating i can not change it right now
-// gix the problem with _id
-// i should change the routes design , like for admin it should  admin/posts , admin/orders , its better
-// i know that all-orders and sales should be seperated but i think its better to put them in one page because its not a big deal
